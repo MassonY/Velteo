5dda177d15f67c2e7c5f179884cd71ca42f0ace8
hadesfufu <yohan.masson@neuf.fr>
2016-11-10 08:20:09 +0100

    Layout init

---
+++ resources/views/general.blade.php
@@ -0,0 +1,21 @@
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
+        <title>Velteo</title>
+        <link href="{{ asset('/css/velteo_general.css') }}" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"/>
+    </head>
+    <header>
+        <div id="title">
+            <h1>Velteo</h1>
+        </div>
+    </header>
+    <body>
+        @yield('content')
+    </body>
+    <footer>
+
+    </footer>
+</html>
